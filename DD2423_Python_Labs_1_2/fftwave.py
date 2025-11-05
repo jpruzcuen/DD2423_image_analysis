a758c038451d2d53fa000f386ce49d28ba08feaf
Kajlid <104261817+Kajlid@users.noreply.github.com>
2025-11-05 13:52:31 +0100

    Assignment 4: replace wavelength expression

--- DD2423_Python_Labs_1_2/fftwave.py
+++ DD2423_Python_Labs_1_2/fftwave.py
@@ -26,7 +26,7 @@ def fftwave(u, v, sz = 128):
 	else:
 		vc = v - sz
 
-	wavelength = 0.0 # Replace by correct expression
+	wavelength = 1 / np.sqrt(u ** 2 + v ** 2) # Replace by correct expression
 	amplitude  = 1 / sz  # Replace by correct expression
 	
 	a2 = f.add_subplot(3, 2, 2)
