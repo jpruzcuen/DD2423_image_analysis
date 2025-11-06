Update amplitude formula to correct one

DD2423_Python_Labs_1_2/fftwave.py
@@ -27,7 +27,7 @@ def fftwave(u, v, sz = 128):
 		vc = v - sz
 
 	wavelength = 1 / np.sqrt(u ** 2 + v ** 2) # Replace by correct expression
-	amplitude  = 1 / sz  # Replace by correct expression
+	amplitude  = 1 / sz ** 2  # Replace by correct expression
 	
 	a2 = f.add_subplot(3, 2, 2)
 	showgrey(np.fft.fftshift(Fhat), False)      # fftshift shifts the zero-frequency component to the center of the spectrum